7f64e3e3ec37bead84d73a49b0f17ccdecda3b1b
Felipe <FelipeCll01@outlook.com>
2022-09-06 10:02:12 -0300

    Add bg and header function

--- js/game.js
+++ js/game.js
@@ -1,4 +1,6 @@
 const grid = document.querySelector('.grid');
+const spanPlayer = document.querySelector('.player');
+const timer = document.querySelector('.timer');
 
 const characters = [
     'Eric',
@@ -18,7 +20,8 @@ const checkEndGame = () => {
     const disabledCards = document.querySelectorAll('.disabled-card');
 
     if (disabledCards.length == 4) {
-        alert ('Parabéns');
+        clearInterval(this.loop);
+        alert (`Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi: ${timer.innerHTML}`);
     }
 }
 
@@ -100,4 +103,19 @@ const loadGame = () => {
         grid.appendChild(card);
     });
 }
-loadGame();
+
+const startTimer = () => {
+
+    this.loop = setInterval(() => {
+        const currentTime = +timer.innerHTML;
+        timer.innerHTML = currentTime + 1;
+    }, 1000);
+}
+
+window.onload = () => {
+
+    spanPlayer.innerHTML = localStorage.getItem('player');
+    startTimer();
+    loadGame();
+}
+
